33fdbb37aa8ecbb080dd2608a2170fee7177c5ec
Nurlan <nurlaney@code.edu.az>
2021-05-18 02:29:37 +0400

    Added swapping selectbox and error message

--- currency/src/components/converter/Converter.js
+++ currency/src/components/converter/Converter.js
@@ -7,11 +7,16 @@ class Converter extends Component {
     selectRef = React.createRef();
     outputSelectRef = React.createRef();
     outputRef = React.createRef();
+    errorMessageRef = React.createRef();
     
-
     exchange(){
-        const calculated = this.calculateCurrency(this.selectRef.current.value,this.outputSelectRef.current.value);
-        this.outputRef.current.textContent = this.rounder(calculated);
+        if(this.selectRef.current.value == 'default' || this.outputSelectRef.current.value == 'default'){
+            this.errorMessageRef.current.classList.remove('d-none');
+        }else{
+            const calculated = this.calculateCurrency(this.selectRef.current.value,this.outputSelectRef.current.value);
+            this.outputRef.current.textContent = this.rounder(calculated);
+            this.errorMessageRef.current.classList.add('d-none');
+        }
     }
 
     calculateCurrency(first,second){
@@ -22,6 +27,14 @@ class Converter extends Component {
 
     rounder(num){
         return Math.round(num * 100) / 100;
+    }
+
+    swapSelects(){
+        let temp;
+        temp = this.selectRef.current.value;
+        this.selectRef.current.value = this.outputSelectRef.current.value;
+        this.outputSelectRef.current.value = temp;
+        this.exchange();
     }
 
     render() {
@@ -35,7 +48,8 @@ class Converter extends Component {
                         <div className='top'>
                             <p>From :</p>
                             <input ref={this.inputRef} type='number' placeholder='Set amount'/>
-                            <select ref={this.selectRef}>
+                            <select ref={this.selectRef} defaultValue={'default'}>
+                            <option disabled value={'default'}>Select</option>
                                 {currency.map((item, value) => (
                                     <option key={value}>{item.code}</option>
                                 ))}
@@ -47,13 +61,15 @@ class Converter extends Component {
                         <div className='bottom'>
                             <p>To :</p>
                             <span><b ref={this.outputRef}>0</b></span>
-                            <select ref={this.outputSelectRef}>
+                            <select ref={this.outputSelectRef} defaultValue={'default'}>
+                            <option disabled value={'default'}>Select</option>
                                 {currency.map((item, value) => (
                                     <option key={value}>{item.code}</option>
                                 ))}
                             </select>
                         </div>
                         <button onClick={this.exchange.bind(this)}>Exchange</button>
+                        <p ref={this.errorMessageRef} className='errorMessage text-danger d-none'>You have to select currency</p>
                     </div>
                 </div>
             </div>
