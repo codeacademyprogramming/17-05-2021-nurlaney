45258b130d8399b5e78ebac4d1d0ce5ba59c1d90
Nurlan <nurlaney@code.edu.az>
2021-05-18 01:55:00 +0400

    Added calculating currencies method

--- currency/src/components/converter/Converter.js
+++ currency/src/components/converter/Converter.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import currency from '../../rates.json';
 
 class Converter extends Component {
+    //references
+    inputRef = React.createRef();
+    selectRef = React.createRef();
+    outputSelectRef = React.createRef();
+    outputRef = React.createRef();
+    
+
+    exchange(){
+        const calculated = this.calculateCurrency(this.selectRef.current.value,this.outputSelectRef.current.value);
+        this.outputRef.current.textContent = this.rounder(calculated);
+    }
+
+    calculateCurrency(first,second){
+        const firstSelect = currency.find(x => x.code == first).value;
+        const secondSelect = currency.find(x => x.code == second).value;
+        return (firstSelect/secondSelect) * this.inputRef.current.value;
+    }
+
+    rounder(num){
+        return Math.round(num * 100) / 100;
+    }
+
     render() {
         return (
             <div>
@@ -11,27 +33,27 @@ class Converter extends Component {
                     </div>
                     <div className='converter-card'>
                         <div className='top'>
-                            <p>From</p>
-                            <input type='number' />
-                            <select>
+                            <p>From :</p>
+                            <input ref={this.inputRef} type='number' placeholder='Set amount'/>
+                            <select ref={this.selectRef}>
                                 {currency.map((item, value) => (
                                     <option key={value}>{item.code}</option>
                                 ))}
                             </select>
                         </div>
                         <div className='middle text-center'>
-                            <img src={`${process.env.PUBLIC_URL}/imgs/switch.png`} />
+                            <img onClick={this.swapSelects.bind(this)} src={`${process.env.PUBLIC_URL}/imgs/switch.png`} />
                         </div>
                         <div className='bottom'>
-                            <p>To</p>
-                            <span><b>1700</b></span>
-                            <select>
+                            <p>To :</p>
+                            <span><b ref={this.outputRef}>0</b></span>
+                            <select ref={this.outputSelectRef}>
                                 {currency.map((item, value) => (
                                     <option key={value}>{item.code}</option>
                                 ))}
                             </select>
                         </div>
-                    <button>Exchange</button>
+                        <button onClick={this.exchange.bind(this)}>Exchange</button>
                     </div>
                 </div>
             </div>
